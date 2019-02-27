cf443bfe242614a36fe6cdd0fde74f925a085b92
Severi Tikkanen <severitikkanen@gmail.com>
2019-02-27 12:44:58 +0200

    Added material styling

--- src/components/SearchResultsList.js
+++ src/components/SearchResultsList.js
@@ -7,13 +7,18 @@ const SearchResultsList = ({term}) => {
   const arrayOfPlayers = searchPlayers(term);
   // Returns array of player statistics
   const parsedPlayerIds = parseSearchResult(arrayOfPlayers);
+  console.log(parsedPlayerIds);
 
   const renderPlayerList = () => {
-    return parsedPlayerIds.map(player => <li key={player[0]}>{`${player[2]} ${player[1]}`}</li>);
+    return parsedPlayerIds.map(player =>
+      <li className="mdc-list-item" key={player[0]}>
+        <span className="mdc-list-item__text">{`${player[2]} ${player[1]}, ${player[8]}`}</span>
+      </li>
+    );
   };
 
   return (
-    <ul>
+    <ul className="mdc-list">
       {renderPlayerList()}
     </ul>
   );
