deb057ba325e95ee87369173ce04e757a7ca5fc0
Severi Tikkanen <severitikkanen@gmail.com>
2019-02-28 21:08:31 +0200

    Reading for mdc to material-ui

--- src/components/SearchResultsList.js
+++ src/components/SearchResultsList.js
@@ -9,27 +9,22 @@ const SearchResultsList = ({ term }) => {
   const parsedPlayerIds = parseSearchResult(arrayOfPlayers);
   console.log(parsedPlayerIds);
 
-  const renderPlayerList = () => {
-    return parsedPlayerIds.map(player => (
-      <div>
-        {/* 
-        https://nhl.bamcontent.com/images/actionshots/${PLAYERID}
-      */}
-        <li className="mdc-list-item" key={player[0]}>
-          <span className="mdc-list-item__text">
-            {`${player[2]} ${player[1]}, ${player[8]}`}
-          </span>
-        </li>
-        <li
-          role="separator"
-          className="mdc-list-divider"
-          key={player[13] ? player[13] : player[12]}
-        />
-      </div>
-    ));
-  };
+  const renderPlayerList = parsedPlayerIds.map(player => (
+    <div>
+      {/*
+      https://nhl.bamcontent.com/images/headshots/current/168x168/${playerID}.jpg
+    */}
+      <li className="mdc-list-item" tabIndex="0" key={player[0]}>
+        <span className="mdc-list-item__text">
+          <span className="mdc-list-item__primary-text">{player[2] + ' ' + player[1]}</span>
+          <span className="mdc-list-item__secondary-text">Second-line text</span>
+        </span>
+      </li>
+      <hr className="mdc-list-divider"/>
+    </div>
+  ));
 
-  return <ul className="mdc-list">{renderPlayerList()}</ul>;
+  return <ul className="mdc-list mdc-list--two-line">{renderPlayerList}</ul>;
 };
 
 export default SearchResultsList;
