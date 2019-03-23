1eb1ff99b449b5e440e12fbe4f2be377ad2ca8c6
Severi Tikkanen <severitikkanen@gmail.com>
2019-03-23 17:46:50 +0200

    Added commenting on jss

--- src/styles/jss-styles.js
+++ src/styles/jss-styles.js
@@ -1,5 +1,15 @@
 import { fade } from '@material-ui/core/styles/colorManipulator';
 
+/* 
+
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+var isFirefox = typeof InstallTrigger !== 'undefined';
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+var isChrome = !!window.chrome && !isOpera; 
+var isIE = false || !!document.documentMode;
+
+*/
+
 let isFirefox = typeof InstallTrigger !== 'undefined';
 
 export const searchPlayerBarStyles = theme => ({
