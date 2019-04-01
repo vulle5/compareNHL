af62e0aaa93810322c617b98f534b73a5f8abb2a
severi.tikkanen <severitikkanen@gmail.com>
2019-04-01 14:17:50 +0300

    Fixed list key problems

--- src/components/StatTable.js
+++ src/components/StatTable.js
@@ -9,19 +9,18 @@ import {
 import { withStyles } from "@material-ui/core/styles";
 import { seasonTableStyles } from "../styles/jss-styles";
 
-const generateTableHead = headCells => {
-  return headCells.map(cell => (
-    <TableCell align="center">{cell}</TableCell>
+const generateTableHead = (headCells) => {
+  return headCells.map((cell, index) => (
+    <TableCell align="center" key={index}>{cell}</TableCell>
   ));
 };
 
-const generateTableBody = (bodyCells, tableCells, classes, key) => {
-  // TODO: Make unique key for each prop
+const generateTableBody = (bodyCells, tableCells, classes) => {
   const newArray = tableCells.map(object => {
     return Object.values(object);
   });
-  return bodyCells.map((cell) => (
-    <TableRow key={key || cell[0]}>
+  return bodyCells.map((cell, index) => (
+    <TableRow key={index}>
       {newArray.splice(0, 1).map(cell => (
         cell.splice(1).map((oneCell, index) => (
           <TableCell align="center" key={index} className={classes.rowItem}>
@@ -33,7 +32,7 @@ const generateTableBody = (bodyCells, tableCells, classes, key) => {
   ));
 };
 
-const StatTable = ({ classes, headCells, bodyCells, tableCells, key }) => {
+const StatTable = ({ classes, headCells, bodyCells, tableCells }) => {
   return (
     <div>
       <Table className={classes.table}>
@@ -43,7 +42,7 @@ const StatTable = ({ classes, headCells, bodyCells, tableCells, key }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {generateTableBody(bodyCells, tableCells, classes, key)}
+          {generateTableBody(bodyCells, tableCells, classes)}
         </TableBody>
       </Table>
     </div>
