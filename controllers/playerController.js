89ba5d6c421c327562480c01fe7fde31377339cc
severi.tikkanen <severitikkanen@gmail.com>
2019-06-01 20:35:15 +0300

    Added server side fetch template

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,7 +1,22 @@
 const playerRoutes = require("express").Router();
+const fetch = require("node-fetch");
 
-playerRoutes.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+const url = "https://statsapi.web.nhl.com/api/v1/people/8478427";
+
+const getData = async url => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+playerRoutes.get("/", async (req, res) => {
+  const response = await getData(url);
+  res.json(response);
 });
 
 module.exports = playerRoutes;
