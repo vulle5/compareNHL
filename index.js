a94833cf2970f4bc8b391bf097e0aef560e9a792
severi.tikkanen <severitikkanen@gmail.com>
2019-06-02 15:58:01 +0300

    Reading for heroku deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const server = http.createServer(app);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
