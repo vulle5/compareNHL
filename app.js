386e62b251e05ddc873c76b114b571ffb6e2f502
severi.tikkanen <severitikkanen@gmail.com>
2019-06-02 19:17:51 +0300

    Fixed missing dependence issue preventing deployment

--- app.js
+++ app.js
@@ -8,4 +8,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/players", playerRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 module.exports = app;
--- index.js
+++ index.js
@@ -3,15 +3,6 @@ const http = require("http");
 
 const server = http.createServer(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
