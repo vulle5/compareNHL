41d538d981c25cd91b3e9e23c483897f0f8b1c54
severi.tikkanen <severitikkanen@gmail.com>
2019-06-07 17:17:07 +0300

    FAB now disapears on mobile if at the bottom of a page

--- client/src/components/FAB.js
+++ client/src/components/FAB.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import _ from "lodash";
 import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import withWidth, { isWidthUp } from "@material-ui/core/withWidth";
-import Fab from "@material-ui/core/Fab";
+import { Fab, Zoom } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
 import { floatingActionButtonStyles } from "../styles/jss-styles";
@@ -15,22 +16,53 @@ const FloatingActionButton = ({
   title,
   isLink
 }) => {
+  const [atBottom, setAtBottom] = useState(false);
+
+  const handleScroll = _.throttle(
+    useCallback(() => {
+      const winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop;
+
+      const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const scrolled = winScroll / height;
+
+      if (scrolled === 1) {
+        setAtBottom(true);
+      } else {
+        setAtBottom(false);
+      }
+    }, []),
+    200
+  );
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
     <div className={classes.root}>
-      <Fab
-        variant={isWidthUp("sm", width) ? "extended" : "round"}
-        aria-label="Add to Compare"
-        className={classes.fab}
-        color="primary"
-        component={isLink ? Link : "button"}
-        to={to}
-        onClick={onClick && isLink === undefined ? onClick : null}
-      >
-        <AddIcon
-          className={isWidthUp("sm", width) ? classes.extendedIcon : null}
-        />
-        {isWidthUp("sm", width) ? title : null}
-      </Fab>
+      <Zoom in={isWidthUp("sm", width) ? true : !atBottom}>
+        <Fab
+          variant={isWidthUp("sm", width) ? "extended" : "round"}
+          aria-label="Add to Compare"
+          className={classes.fab}
+          color="primary"
+          component={isLink ? Link : "button"}
+          to={to}
+          onClick={onClick && isLink === undefined ? onClick : null}
+        >
+          <AddIcon
+            className={isWidthUp("sm", width) ? classes.extendedIcon : null}
+          />
+          {isWidthUp("sm", width) ? title : null}
+        </Fab>
+      </Zoom>
     </div>
   );
 };
