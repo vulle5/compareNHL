7b6c9b36bafa89d666d46a172dcf9a728302e3ed
severi.tikkanen <severitikkanen@gmail.com>
2019-07-01 15:32:39 +0300

    Compare is now integrated with redux

--- client/src/components/Compare.js
+++ client/src/components/Compare.js
@@ -1,25 +1,22 @@
 import React from "react";
-import _ from "lodash";
+import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
-import { CircularProgress, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 
 import { compareStyles } from "../styles/jss-styles";
-import { genPlayer } from "../functions/genPlayer";
-import { useGetPlayerInfo } from "../functions/useGetPlayerInfo";
 
-const Compare = ({ match: { params }, classes }) => {
-  const playerInfo = useGetPlayerInfo(
-    params.playerId,
-    "?expand=person.stats&stats=yearByYear,careerRegularSeason&expand=stats.team"
-  );
-
-  if (_.get(playerInfo, "people[0]")) {
-    const player = genPlayer(playerInfo.people[0]);
-    console.log(player);
-    return <Typography variant="h3">{player.currentTeam.name}</Typography>;
-  } else {
-    return <CircularProgress />;
-  }
+const Compare = ({ match: { params }, classes, player }) => {
+  console.log(player);
+  return <Typography variant="h3">{player.currentTeam.name}</Typography>;
 };
 
-export default withStyles(compareStyles)(Compare);
+const mapStateToProps = state => {
+  return {
+    player: state.player
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(withStyles(compareStyles)(Compare));
