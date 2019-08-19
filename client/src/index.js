81b09648ff181a2a9f21b8601f0bbfe868b55a3b
Severi Tikkanen <severitikkanen@gmail.com>
2019-08-19 15:46:06 +0300

    Added themeProvider and changed default theme

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors/";
+
 import App from "./components/App";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#0d47a1"
+    },
+    secondary: red
+  }
+});
 
 const render = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
     </Provider>,
     document.getElementById("root")
   );
