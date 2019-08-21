f8dd8ccaf0fa9b8964979449a8e88c854de07696
Severi Tikkanen <severitikkanen@gmail.com>
2019-08-21 15:26:36 +0300

    made testReduxRenderer into its own function

--- client/src/components/__tests__/App.test.js
+++ client/src/components/__tests__/App.test.js
@@ -1,26 +1,9 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import { render, cleanup } from "@testing-library/react";
-import { reducers } from "../../store";
-import App from "../App";
+import { cleanup } from "@testing-library/react";
+import { renderWithRedux } from "../../functions/renderWithRedux";
 
-// this is a handy function that I normally make available for all my tests
-// that deal with connected components.
-// you can provide initialState or the entire store that the ui is rendered with
-function renderWithRedux(
-  ui,
-  { initialState, store = createStore(reducers, initialState) } = {}
-) {
-  return {
-    ...render(<Provider store={store}>{ui}</Provider>),
-    // adding `store` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
-    store
-  };
-}
+import App from "../App";
 
 afterEach(cleanup);
 
---
+++ client/src/functions/renderWithRedux.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { render, cleanup } from "@testing-library/react";
+import { reducers } from "../store";
+
+// this is a handy function that I normally make available for all my tests
+// that deal with connected components.
+// you can provide initialState or the entire store that the ui is rendered with
+export function renderWithRedux(
+  ui,
+  { initialState, store = createStore(reducers, initialState) } = {}
+) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    // adding `store` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    store
+  };
+}
