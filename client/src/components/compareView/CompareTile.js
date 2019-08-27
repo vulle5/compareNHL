a4532944e59c65377701db4e81e9ac5af8ee9dad
Severi Tikkanen <severitikkanen@gmail.com>
2019-08-27 14:02:38 +0300

    Filter added for compare non functional

--- client/src/components/compareView/CompareTile.js
+++ client/src/components/compareView/CompareTile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { find } from "lodash";
 import { Paper, IconButton } from "@material-ui/core";
 import ClearIcon from "@material-ui/icons/Clear";
 
@@ -8,18 +9,20 @@ import { useCompareStyles } from "../../styles/useStyles";
 import { makeCompareData } from "../../functions/makeCompareData";
 import CompareTileItem from "./CompareTileItem";
 import CompareTileHeader from "./CompareTileHeader";
+import DisplayFilter from "../DisplayFilter";
+import CareerFilter from "../CareerFilter";
 
 const CompareTile = ({
   player,
   removeCompare,
   compare,
-  compareCareerRegular
+  compareCareerRegular,
+  playerSeasons
 }) => {
   const listItems = compareCareerRegular
     ? makeCompareData(compareCareerRegular)
     : null;
   const classes = useCompareStyles();
-
   return (
     <Paper className={classes.tileRoot}>
       {compare.length > 1 && (
@@ -32,6 +35,10 @@ const CompareTile = ({
       )}
       <div className={classes.tileWrapper}>
         <CompareTileHeader player={player} />
+        <div style={{ textAlign: "center" }}>
+          <CareerFilter filterKey="compareTile" filterNames={playerSeasons} />
+          <DisplayFilter />
+        </div>
         {listItems ? (
           <CompareTileItem listItems={listItems} />
         ) : (
@@ -42,9 +49,28 @@ const CompareTile = ({
   );
 };
 
-const mapStateToProps = state => {
+const createFilters = (compare, id) =>
+  compare
+    .filter(player => player.id === id)
+    .map(({ stats: { 0: { splits } } }) =>
+      splits
+        .filter(season => season.league.name === "NHL")
+        .map(season => season.season.slice(0, 4) + "-" + season.season.slice(4))
+    )
+    .flat();
+
+const getSelectedSeason = (allSeasons, selectedFilter) =>
+  find(allSeasons.filter(season => season.league.name === "NHL"), {
+    season: selectedFilter
+  });
+
+const mapStateToProps = (state, ownProps) => {
+  const { compare } = state;
+  const { player } = ownProps;
   return {
-    compare: state.compare
+    compare,
+    // filteredSeasons: getSelectedSeason()
+    playerSeasons: createFilters(compare, player.id)
   };
 };
 
