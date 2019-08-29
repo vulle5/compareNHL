6f077d5b5069cc8b2ef28eaa90232e34a46f00cd
Severi Tikkanen <severitikkanen@gmail.com>
2019-08-29 19:49:44 +0300

    Added text compression for files that are served to heroku

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   const path = require("path");
+  const compression = require("compression");
+
+  app.use(compression());
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
--- client/src/components/appBar/SearchPlayersBar.js
+++ client/src/components/appBar/SearchPlayersBar.js
@@ -69,7 +69,6 @@ const SearchPlayersBar = props => {
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
-
               <InputBase
                 autoFocus
                 autoComplete="off"
