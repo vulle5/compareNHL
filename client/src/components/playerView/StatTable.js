9eea058932cc848e2c0e0474fa0ac6e180686caa
Severi Tikkanen <severitikkanen@gmail.com>
2019-09-09 14:00:01 +0300

    TableCells are now equal in size on firefox

--- client/src/components/playerView/StatTable.js
+++ client/src/components/playerView/StatTable.js
@@ -25,7 +25,11 @@ const generateTableBody = (bodyCells, tableCells, classes) => {
     <TableRow key={index}>
       {newArray.splice(0, 1).map(cell =>
         cell.splice(1).map((oneCell, index) => (
-          <TableCell align="center" style={{ overflow: "hidden" }} key={index}>
+          <TableCell
+            align="center"
+            style={{ overflow: "hidden", height: "60px" }}
+            key={index}
+          >
             {oneCell}
           </TableCell>
         ))
