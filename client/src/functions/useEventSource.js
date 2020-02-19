4df9fcb6a2b58c5bff1e33dad25d1dbb1614af54
Severi Tikkanen <severitikkanen@gmail.com>
2020-02-19 18:12:42 +0200

    Fixed useEventSource hook

--- client/src/functions/useEventSource.js
+++ client/src/functions/useEventSource.js
@@ -6,14 +6,20 @@ const useEventSource = (
   { onMessage = null, onError = null, onOpen = null } = {}
 ) => {
   const [event, setEvent] = useState(null);
-  const eventSource = useRef(new EventSource(src));
-  eventSource.current.onmessage = onMessage;
-  eventSource.current.onerror = onError;
-  eventSource.current.onopen = onOpen;
+  const eventSource = useRef(null);
 
   const onEvent = useCallback(e => {
     setEvent(e);
   }, []);
+
+  useEffect(() => {
+    if (!eventSource.current) {
+      eventSource.current = new EventSource(src);
+    }
+    eventSource.current.onmessage = onMessage;
+    eventSource.current.onerror = onError;
+    eventSource.current.onopen = onOpen;
+  }, [onError, onMessage, onOpen, src]);
 
   useEffect(() => {
     const eventRef = eventSource.current;
